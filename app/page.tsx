44a2f4ddb187f4ffac519b8067b66ae1f8ef41c7
Lucas Fernandes Ribeiro de Oliveira <136923413+lucas-fro@users.noreply.github.com>
2025-12-02 22:00:15 -0300

    arruamando a tela do site

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-background">
       <Header/>  
-      <section className="min-h-dvh flex flex-wrap gap-5 p-10 items-center w-full">
+      <section className="min-h-dvh flex flex-wrap gap-5 md:p-10 p-5 items-center w-full">
 
         <div className="flex-1 grid md:gap-10 gap-5 text-center md:text-left">
-          <h1 className="mt-5 text-foreground md:text-6xl text-5xl font-bold">
+          <h1 className="md:mt-5 mt-10 text-foreground md:text-6xl text-4xl font-bold">
             Descubra tudo sobre suas <span className="text-primary">conversas do WhatsApp</span>
           </h1>
 
-          <p className="text-border md:text-2xl text-lg">
+          <p className="text-border md:text-2xl text-md">
             Transforme suas conversas em dados fascinantes. Veja quem mais fala, horários mais ativos,
             emojis favoritos e muito mais.
           </p>
 
-          <div className="flex flex-wrap justify-center lg:justify-start gap-5">
+          <div className="flex flex-wrap justify-center lg:justify-start  md:gap-5 gap-3">
             <Tags icon={CircleCheck} text="sem cadastro" />
             <Tags icon={Shield} text="Processamento local" />
             <Tags icon={Zap} text="Resultados instantâneos" />
